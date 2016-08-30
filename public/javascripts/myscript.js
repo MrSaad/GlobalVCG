699b77a2d765328d74004012b2ff3146697d67a1
Saad Ahmed <saadem3000@gmail.com>
2016-08-30 07:47:30 -0400

    Update colours and styles

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -2,69 +2,76 @@ $(function(){
 
 	"use strict"
 	
-		var topoffset=0
-		var wheight = $(window).height(); //height of window
-	
-		//set height of main page 
-		$('.page#main').css('height', wheight);
+	var topoffset=0
+	var wheight = $(window).height(); //height of window
+	var aspectRatio = $(window).width()/$(window).height(); //aspect ratio
+
+	//set height of main page 
+	$('.page#main').css('height', wheight);
+	$('.particles-js-canvas-el').css('height', $('#about').height());
+	//adjust height of .fullheight elements on window resize
+	$(window).resize(function() {
+		wheight = $(window).height(); //get the height of the window
+		$('.page#main').css('height', wheight); //set to window tallness  
 		$('.particles-js-canvas-el').css('height', $('#about').height());
-		//adjust height of .fullheight elements on window resize
-		$(window).resize(function() {
-			wheight = $(window).height(); //get the height of the window
-			$('.page#main').css('height', wheight); //set to window tallness  
-			$('.particles-js-canvas-el').css('height', $('#about').height());
-		});
-	
-		 //Activate Scrollspy
-		$('body').scrollspy({target: '.navbar', offset: 100});
-	
-		// add inbody class
+	});
+
+	console.log(aspectRatio+": "+(1920.0/1280));
+	if( aspectRatio < (1920.0/1280) ){
+		console.log(aspectRatio+": "+(1920.0/1280));
+		$("#main").css({"background-size": "auto 100%"});
+	}
+
+	 //Activate Scrollspy
+	$('body').scrollspy({target: '.navbar', offset: 100});
+
+	// add inbody class
+	var hash = $(this).find('li.active a').attr('href');
+	if(hash !== '#main') {
+		$('header nav').addClass('inbody');
+	} else {
+		$('header nav').removeClass('inbody');
+	}
+
+	// Add an inbody class to nav when scrollspy event fires
+	$('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
 		var hash = $(this).find('li.active a').attr('href');
-		if(hash !== '#main') {
+	    if(hash !== '#main') {
+	    // if ($(window).scrollTop() > 20){
 			$('header nav').addClass('inbody');
-		} else {
+	    } else {
 			$('header nav').removeClass('inbody');
-		}
-	
-		// Add an inbody class to nav when scrollspy event fires
-		$('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
-			var hash = $(this).find('li.active a').attr('href');
-		    if(hash !== '#main') {
-		    // if ($(window).scrollTop() > 20){
-				$('header nav').addClass('inbody');
-		    } else {
-				$('header nav').removeClass('inbody');
-		    }
-		});
-	
-		$('.fa-angle-down').click(function(){
-			$('html,body').animate({
-				scrollTop: $('#about').offset().top
-	        }, 500);
-		});
-	
-		//Use smooth scrolling when clicking on navigation
-		$('.navbar a[href*=#]:not([href=#])').click(function() {
-		    if (location.pathname.replace(/^\//,'') === 
-				this.pathname.replace(/^\//,'') && 
-				location.hostname === this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				if (target.length) {
-					$('html,body').animate({
-						scrollTop: target.offset().top-topoffset+2
-			        }, 500);
-			    	return false;
-				} //target.length
-		    } //click function
-		}); //smooth scrolling)
+	    }
+	});
+
+	$('.fa-angle-down').click(function(){
+		$('html,body').animate({
+			scrollTop: $('#about').offset().top
+        }, 500);
+	});
+
+	//Use smooth scrolling when clicking on navigation
+	$('.navbar a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') === 
+			this.pathname.replace(/^\//,'') && 
+			location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top-topoffset+2
+		        }, 500);
+		    	return false;
+			} //target.length
+	    } //click function
+	}); //smooth scrolling)
 
 	
 
 	//add parallax to first div
-	$('.bg-1').parallax({
-		speed :	0.25
-	});
+	// $('.bg-1').parallax({
+	// 	speed :	0.25
+	// });
 
 });
 
