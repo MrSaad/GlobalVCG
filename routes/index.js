c0806cb31eab1cc13bebc408c64e1cb078e41581
Saad Ahmed <saadem3000@gmail.com>
2016-09-18 06:39:14 -0400

    Add compliments route

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Global Vehicle Consulting Group LLC' });
 });
 
+var compliments = ["You're doing great!",
+					"Keep it up dude!",
+					"You're an awesome person!"]
+
+/* GET compliment page. */
+router.get('/compliment', function(req, res, next) {
+	var randomIndex = Math.floor(Math.random() * compliments.length); 
+	res.send(compliments[randomIndex]);
+});
+
 /* POST send message */
 router.post('/sendmessage', function(req, res, next){
 	console.log("received");
