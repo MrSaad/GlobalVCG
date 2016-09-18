d8c0d7a7215df7fa1acd791a62ad70c5f232dfce
Saad Ahmed <saadem3000@gmail.com>
2016-09-18 06:50:36 -0400

    Move compliments to separate file

---
+++ models/compliments.js
@@ -0,0 +1,6 @@
+module.exports = {
+    compliments: ["You're doing great!",
+          "Keep it up dude!",
+          "You're an awesome person!",
+        ]
+}
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var compliments = require('../models/compliments');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,11 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Global Vehicle Consulting Group LLC' });
 });
 
-var compliments = ["You're doing great!",
-					"Keep it up dude!",
-					"You're an awesome person!"]
-
 /* GET compliment page. */
+var compliments = compliments.compliments;
 router.get('/compliment', function(req, res, next) {
 	var randomIndex = Math.floor(Math.random() * compliments.length); 
 	res.send(compliments[randomIndex]);
